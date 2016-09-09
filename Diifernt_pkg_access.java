f254136c69dee6a1e27e0eba19964a4d0c0b852f
KarthikNageswaran <karthick.information@gmail.com>
2016-09-09 20:14:20 +0530

    Add files via upload

---
+++ Diifernt_pkg_access.java
@@ -0,0 +1,16 @@
+import Testing.*;
+
+public class Diifernt_pkg_access {
+
+	public static void main(String[] args) {
+		
+		Logout l1=new Logout();
+		l1.logout();
+		
+		Test1 t1=new Test1();
+		t1.test_login();
+		
+
+	}
+
+}
